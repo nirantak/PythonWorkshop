028d5bc07570a6b2efbce91fcf70c4ecafac2cb1
nirantak <nirantak.s.r@gmail.com>
2017-08-09 15:24:04 +0530

    First Commit

---
+++ module1.py
@@ -0,0 +1,8 @@
+# print("This will always be displayed.")
+print(__name__)
+
+# def main():
+# 	print("Module 1 name {}".format(__name__))
+
+# if __name__ == '__main__':
+# 	main()
---
+++ module2.py
@@ -0,0 +1,7 @@
+import module1
+
+def main():
+	print("Module 2 name {}".format(__name__))
+
+if __name__ == '__main__':
+	main()
